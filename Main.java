c7002ad7b61c2c5e7eabe344ce523bc6ceb8d6a8
preraku@gmail.com <pupadhyaya6@gatech.edu>
2016-04-06 22:13:46 -0400

    Grammar.

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.math2605;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fuck Y'all");
+        System.out.println("Fuck you all.");
     }
 }
