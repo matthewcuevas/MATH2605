b4dd778afa4beb7345b32991c2ccfd1ab81c3caa
Anush Mattapalli <anush0620@yahoo.com>
2016-04-10 17:02:29 -0400

    Multiply method added and minor changes to others. Will test later

--- src/Matrix.java
+++ src/Matrix.java
@@ -43,37 +43,11 @@ public class Matrix {
      */
     public void set(int row, int column, double num) {
         if (row < 0 || row >= rows || column < 0 || column >= columns) {
-            throw new IllegalArgumentException("Tried to set value at row " + row
-                    + " and column " + column + " of a " + rows + " by " +  columns
-                    + "matrix.");
+            throw new IllegalArgumentException("Try setting a value " +
+                    "that's actually in the matrix. Remember (0,0) is " +
+                    "the first element");
         }
         matrix[row][column] = num;
-    }
-
-    /**
-     * Returns the sum of two matrices
-     * @param m1 a Matrix
-     * @param m2 another Matrix
-     * @return a matrix object that represents the sum of the two matrices given
-     * @throws IllegalArgumentException if matrices have different dimensions
-     */
-    public static Matrix sum(Matrix m1, Matrix m2) {
-        if (m1 == null || m2 == null) {
-            throw new IllegalArgumentException("Tried to add one or more null " +
-                    "matrices.");
-        }
-        if (m1.rows != m2.rows || m1.columns != m2.columns) {
-            throw new IllegalArgumentException("Tried to add a " + m1.rows
-                    + " by " + m1.columns + " matrix to a " + m2.rows
-                    + " by " + m2.columns + " matrix.");
-        }
-        Matrix ret = new Matrix(m1.rows, m1.columns);
-        for (int y = 0; y < ret.rows; y++) {
-            for (int x = 0; x < ret.columns; x++) {
-                ret.set(y, x, m1.get(y, x) + m2.get(y, x));
-            }
-        }
-        return ret;
     }
 
     /**
@@ -90,5 +64,57 @@ public class Matrix {
                     + "matrix.");
         }
         return matrix[row][column];
+    }
+
+    /**
+     * Returns the sum of two matrices
+     * @param m1 a Matrix
+     * @param m2 another Matrix
+     * @return a matrix object that represents the sum of the two matrices given
+     * @throws IllegalArgumentException if matrices have different dimensions
+     */
+    public static Matrix sum(Matrix m1, Matrix m2) {
+        if (m1 == null || m2 == null) {
+            throw new IllegalArgumentException("Matrix might be null lol");
+        }
+
+        //thought this might be helpful
+        if (m1.rows != m2.rows || m1.columns != m2.columns) {
+            throw new IllegalArgumentException("Tried to add a " + m1.rows
+                    + " by " + m1.columns + " matrix to a " + m2.rows
+                    + " by " + m2.columns + " matrix.");
+        }
+        Matrix retur = new Matrix(m1.rows, m1.columns);
+        for (int y = 0; y < retur.rows; y++) {
+            for (int x = 0; x < retur.columns; x++) {
+                retur.set(y, x, m1.get(y, x) + m2.get(y, x));
+            }
+        }
+        return retur;
+    }
+
+    public Matrix multiply(Matrix m1, Matrix m2) {
+        if (m1 == null || m2 == null) {
+            throw new IllegalArgumentException("Matrix might be null lol");
+        }
+        if (m1.columns != m2.rows) {
+            throw new IllegalArgumentException("Tried to multiply a " + m1.rows
+                    + " by " + m1.columns + " matrix by a " + m2.rows
+                    + " by " + m2.columns + " matrix.");
+        }
+        
+        Matrix retur = new Matrix(m1.rows, m2.columns);
+        for (int y = 0; y < m1.rows; y++) {
+            for (int x = 0; x < m2.columns; x++) {
+                double val = 0;
+                for (int z = 0; z < m1.columns; z++) {
+                    val += m1.get(y, z) * m2.get(z, x);
+                }
+                retur.set(y, x, val);
+            }
+        }
+
+
+        return retur;
     }
 }
