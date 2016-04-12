10ca05498b1164db03b582847668319bb94432d7
Matthew Cuevas <matthewxcuevas@gmail.com>
2016-04-12 17:28:53 -0400

    add invert functionality to Matrices

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,5 @@
+package com.math2605.src;
+
 /**
  * Created by Anush on 4/10/2016.
  */
@@ -73,6 +75,7 @@ public class Matrix {
                     "that's actually in the matrix. Remember (0,0) is " +
                     "the first element");
         }
+
         matrix[row][column] = num;
     }
 
@@ -110,6 +113,7 @@ public class Matrix {
                     + " by " + m1.columns + " matrix to a " + m2.rows
                     + " by " + m2.columns + " matrix.");
         }
+
         Matrix retur = new Matrix(m1.rows, m1.columns);
         for (int y = 0; y < retur.rows; y++) {
             for (int x = 0; x < retur.columns; x++) {
@@ -119,10 +123,19 @@ public class Matrix {
         return retur;
     }
 
+    /**
+     * Returns the sum of two matrices
+     * @param m1 a Matrix
+     * @param m2 another Matrix
+     * @return a matrix object that represents the product of the two matrices given
+     * @throws IllegalArgumentException if matrices have incompatible dimensions
+     * or are null
+     */
     public Matrix multiply(Matrix m1, Matrix m2) {
         if (m1 == null || m2 == null) {
             throw new IllegalArgumentException("Matrix might be null lol");
         }
+
         if (m1.columns != m2.rows) {
             throw new IllegalArgumentException("Tried to multiply a " + m1.rows
                     + " by " + m1.columns + " matrix by a " + m2.rows
@@ -144,6 +157,96 @@ public class Matrix {
         return retur;
     }
 
+    /**
+     * Inverts the matrix
+     */
+    public void invert() {
+        double a[][] = matrix;
+        int n = a.length;
+        double x[][] = new double[n][n];
+        double b[][] = new double[n][n];
+        int index[] = new int[n];
+        for (int i=0; i<n; ++i)
+            b[i][i] = 1;
+
+        // Transform the matrix into an upper triangle
+        gaussian(a, index);
+
+        // Update the matrix b[i][j] with the ratios stored
+        for (int i=0; i<n-1; ++i)
+            for (int j=i+1; j<n; ++j)
+                for (int k=0; k<n; ++k)
+                    b[index[j]][k]
+                            -= a[index[j]][i]*b[index[i]][k];
+
+        // Perform backward substitutions
+        for (int i=0; i<n; ++i) {
+            x[n-1][i] = b[index[n-1]][i]/a[index[n-1]][n-1];
+            for (int j=n-2; j>=0; --j) {
+                x[j][i] = b[index[j]][i];
+                for (int k=j+1; k<n; ++k) {
+                    x[j][i] -= a[index[j]][k]*x[k][i];
+                }
+
+                x[j][i] /= a[index[j]][j];
+            }
+        }
+
+        matrix = a;
+    }
+
+    /**
+     * Helper method that uses Gaussian elimination to make the matrix upper
+     * right triangular
+     */
+    public static void gaussian(double a[][], int index[]) {
+        int n = index.length;
+        double c[] = new double[n];
+
+        // Initialize the index
+        for (int i = 0; i < n; ++i)
+            index[i] = i;
+
+        // Find the rescaling factors, one from each row
+        for (int i = 0; i < n; ++i) {
+            double c1 = 0;
+            for (int j = 0; j < n; ++j) {
+                double c0 = Math.abs(a[i][j]);
+                if (c0 > c1) c1 = c0;
+            }
+
+            c[i] = c1;
+        }
+
+        // Search the pivoting element from each column
+        int k = 0;
+        for (int j = 0; j < n - 1; ++j) {
+            double pi1 = 0;
+            for (int i = j; i < n; ++i) {
+                double pi0 = Math.abs(a[index[i]][j]);
+                pi0 /= c[index[i]];
+                if (pi0 > pi1) {
+                    pi1 = pi0;
+                    k = i;
+                }
+            }
+
+            // Interchange rows according to the pivoting order
+            int itmp = index[j];
+            index[j] = index[k];
+            index[k] = itmp;
+            for (int i = j + 1; i < n; ++i) {
+                double pj = a[index[i]][j] / a[index[j]][j];
+
+                // Record pivoting ratios below the diagonal
+                a[index[i]][j] = pj;
+
+                // Modify other elements accordingly
+                for (int l = j + 1; l < n; ++l)
+                    a[index[i]][l] -= pj * a[index[j]][l];
+            }
+        }
+    }
 
 
     @Override
