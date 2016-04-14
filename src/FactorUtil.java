6a7c85b45dc3c74f07906d059ae757eab6a4a37a
Matthew Cuevas <matthewxcuevas@gmail.com>
2016-04-14 02:50:47 -0400

    delete unused code

--- src/FactorUtil.java
+++ src/FactorUtil.java
@@ -34,21 +34,6 @@ public class FactorUtil {
         return LU;
     }
 
-//    /**
-//     * LU Factorize a given Matrix
-//     * @param A the Matrix to be factorized
-//     * @return an array where the first element is L,
-//     * and the second element is U, and the third is the error
-//     */
-//    public static Object[] lu_fact(Matrix A) {
-//        Object[] LU = new Matrix[2];
-//        LU[0] = getL(A);
-//        LU[1] = getU(A);
-//        //TODO: Find ||LU-A||
-//
-//        return LU;
-//    }
-
     /**
      * QR Factorize a given Matrix using the Householders method
      *
@@ -76,44 +61,4 @@ public class FactorUtil {
 
         return QR;
     }
-
-//    /**
-//     * This method returns the "L" of the lu factorization
-//     * @param q the Q matrix to factor/decompose
-//     * @return the decomposed l matrix
-//     */
-//    private static Matrix getL(Matrix q) {
-//        Matrix L = new Matrix(q.getRows(), q.getColumns());
-//        for (int i = 0; i < q.getRows(); i++) {
-//            for (int j = 0; j < q.getColumns(); j++) {
-//                if (i > j) {
-//                    L.set(i, j, q.get(i, j));
-//                } else if (i == j) {
-//                    L.set(i, j, 1.0);
-//                } else {
-//                    L.set(i, j, 0.0);
-//                }
-//            }
-//        }
-//        return L;
-//    }
-//
-//    /**
-//     * This method returns the "U" of the lu factorization
-//     * @param q the Q matrix to factor/decompose
-//     * @return the decomposed u matrix
-//     */
-//    private static Matrix getU(Matrix q) {
-//        Matrix U = new Matrix(q.getRows(), q.getColumns());
-//        for (int i = 0; i < q.getRows(); i++) {
-//            for (int j = 0; j < q.getColumns(); j++) {
-//                if (i <= j) {
-//                    U.set(i, j, q.get(i,j));
-//                } else {
-//                    U.set(i, j, 0.0);
-//                }
-//            }
-//        }
-//        return U;
-//    }
 }
