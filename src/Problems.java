974abe458b979930e9550b8bf1d71a00250450ce
Matthew Cuevas <matthewxcuevas@gmail.com>
2016-04-15 14:47:20 -0400

    finish Part II of Problem 2

--- src/Problems.java
+++ src/Problems.java
@@ -16,6 +16,7 @@ public class Problems {
      * Performs calculations for Problem 2: Convergence of Iterative Methods
      */
     public static void problem_2() {
+        // Part I.
         double[] AData = {1, 1.0/3, 1.0/9, 1.0/3, 1, 1.0/3, 1.0/9, 1.0/3, 1};
         double[] bData = {0.9, 0.1, 0.3};
         double[] xExactData = {39.0 / 40, -13.0/40, 12.0/40};
@@ -91,7 +92,28 @@ public class Problems {
 
         System.out.println("Average ratio of iterations (Jacobi / Gauss-Seidel):");
         System.out.println(iterationRatioAvg);
-        System.out.println("error: " + errorGSAvg);
+        System.out.println("error: " + errorGSAvg + "\n");
+
+        // Part II.
+        double[] A2Data = {1, 2, 2, 1};
+        double[] b2Data = {3, 3};
+        double[] xExact2Data = {1, 1};
+        double[] xNought2Data = {0, 0};
+
+        Matrix A2 = new Matrix(2, 2, A2Data);
+        Vector b2 = new Vector(b2Data);
+        Matrix Ab2 = Matrix.toAugmented(A2, b2);
+        Vector xExact2 = new Vector(xExact2Data);
+        Vector xNought2 = new Vector(xNought2Data);
+
+        double[] errorJacobi = new double[9];
+        double[] errorGS = new double[9];
+        for (int i = 2; i <= 10; i++) {
+            errorJacobi[i - 2] = (double) SolveUtil.jacobi_iter(Ab2,
+                    xNought2, (float) 0.00005, i)[2];
+            errorGS[i - 2] = (double) SolveUtil.gs_iter(Ab2,
+                    xNought2, (float) 0.00005, i)[2];
+        }
     }
 
     /**
