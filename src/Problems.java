30ae5d8a11fcc12db68d316cbfd326af74c85d6f
Matthew Cuevas <matthewxcuevas@gmail.com>
2016-04-15 12:39:08 -0400

    finish Part I of Problem 2

--- src/Problems.java
+++ src/Problems.java
@@ -18,9 +18,11 @@ public class Problems {
     public static void problem_2() {
         double[] AData = {1, 1.0/3, 1.0/9, 1.0/3, 1, 1.0/3, 1.0/9, 1.0/3, 1};
         double[] bData = {0.9, 0.1, 0.3};
+        double[] xExactData = {39.0 / 40, -13.0/40, 12.0/40};
 
         Matrix A = new Matrix(3, 3, AData);
         Vector b = new Vector(bData);
+        Vector xExact = new Vector(xExactData);
 
         Vector[] xNought = new Vector[100];
         Random rand = new Random();
@@ -43,13 +45,15 @@ public class Problems {
         for (int i = 0; i < 3; i++) {
             double sum = 0;
             for (int j = 0; j < 100; j++) {
-                sum += ((Vector) JacobiResults[i][0]).get(i, 0);
+                sum += ((Vector) JacobiResults[j][0]).get(i, 0);
             }
 
             xJacobiData[i] = sum / 100;
         }
         Vector xJacobiAvg = new Vector(xJacobiData);
-        System.out.println(xJacobiAvg);
+
+        double errorJacobiAvg = Vector.sum(xJacobiAvg,
+                xExact.negate()).getNorm();
 
         Object[][] GSResults = new Object[100][3];
         for (int i = 0; i < 100; i++) {
@@ -61,13 +65,33 @@ public class Problems {
         for (int i = 0; i < 3; i++) {
             double sum = 0;
             for (int j = 0; j < 100; j++) {
-                sum += ((Vector) GSResults[i][0]).get(i, 0);
+                sum += ((Vector) GSResults[j][0]).get(i, 0);
             }
 
             xGSData[i] = sum / 100;
         }
         Vector xGSAvg = new Vector(xGSData);
+
+        double errorGSAvg = Vector.sum(xGSAvg,
+                xExact.negate()).getNorm();
+
+        double sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += ((Integer) JacobiResults[i][1]) * 1.0 / (Integer) GSResults[i][1];
+        }
+        double iterationRatioAvg = sum / 100;
+
+        System.out.println("I.\n");
+        System.out.println("Jacobi:");
+        System.out.println(xJacobiAvg);
+        System.out.println("error: " + errorJacobiAvg);
+
+        System.out.println("Gauss-Seidel");
         System.out.println(xGSAvg);
+
+        System.out.println("Average ratio of iterations (Jacobi / Gauss-Seidel):");
+        System.out.println(iterationRatioAvg);
+        System.out.println("error: " + errorGSAvg);
     }
 
     /**
